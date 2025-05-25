39d26cc32836f0ccf6e290d6ff82e65e7fdfa802
Vedangi-11 <vedupatel1103@gmail.com>
2025-05-26 00:27:36 +0530

    useeffect

--- front/src/app/apppanel/page.js
+++ front/src/app/apppanel/page.js
@@ -1,4 +1,6 @@
 'use client';
+export const dynamic = 'force-dynamic';
+
 import { useRouter, useSearchParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
